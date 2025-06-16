81324b771081630d7cb83d0bd01d7d200a8b453f
wldnr1208 <wldnr1208@naver.com>
2025-06-16 15:19:55 +0900

    feat: build 성공

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["three"],
-  webpack: (config: any) => {
-    config.externals = [...config.externals, { canvas: "canvas" }];
+import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
+
+const nextConfig: NextConfig = {
+  transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
+  webpack: (config: Configuration, context: { isServer: boolean }) => {
+    if (!context.isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve?.fallback,
+          fs: false,
+          path: false,
+        },
+      };
+    }
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
